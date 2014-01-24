f492bd2e5e869e7605b85f43b479dde6f9e0c6dd
Sebastian Hebel <sebah1989@gmail.com>
2014-01-24 14:40:57 +0100

    added abusive method to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,14 @@
 class Comment
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :body, type: String
-  
+
   belongs_to :user
   belongs_to :post
   has_many :votes
-  
+
+  def abusive
+    self.votes.inject(0){|sum, vote| sum +=1 if vote.value == -1} >= 3
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,9 @@
 class Vote
   include Mongoid::Document
-  
+  include Mongoid::Timestamps
+
+  field :value, type: Integer
+
   belongs_to :user
   belongs_to :comment
 end
