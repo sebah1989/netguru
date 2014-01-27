5b75df361abf6ed083a9d8e667cfa959f6cc27f5
Sebastian Hebel <sebah1989@gmail.com>
2014-01-27 15:53:50 +0100

    changed marking comment as abusive

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,21 @@
 class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
-
+  
+  
   field :body, type: String
+  field :abusive, type: Boolean, default: false
+
 
   belongs_to :user
   belongs_to :post
   has_many :votes
 
-  def abusive
-    self.votes.inject(0){|sum, vote| sum +=1 if vote.value == -1} >= 3
+  def check_abusive
+    votes = self.votes.inject(0) {|sum, vote| sum +=1 if vote.value == -1}
+    if votes && votes >= 3
+      self.abusive = true
+      self.save
+    end
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,15 @@ class Vote
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  after_create :check_comment
+
   field :value, type: Integer
 
   belongs_to :user
   belongs_to :comment
+
+  protected
+  def check_comment
+  	self.comment.check_abusive
+  end
 end
